6e80fb020d9ddb140a5d338e0611ddd6ada63ab5
Christoph Hack <christoph@tux21b.org>
2012-11-01 15:54:32 +0100

    set windows title

--- main.go
+++ main.go
@@ -1,97 +1,127 @@
 package main
 
 import (
-//    "fmt"
-    "github.com/0xe2-0x9a-0x9b/Go-SDL/sdl"
-    "log"
-    "net"
-//    "math"
-//    "os"
-//    "strings"
-    "time"
-    "math/rand"
+	"encoding/binary"
+	"fmt"
+	"github.com/0xe2-0x9a-0x9b/Go-SDL/sdl"
+	"log"
+	"math"
+	"math/rand"
+	"net"
+	"runtime"
+	"time"
 )
 
 type Game interface {
-    Run()
+	Run()
 }
+
+const (
+	ButtonA = 1
+	ButtonB = 2
+)
 
 func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	log.Println("client connected")
+
+	buf := make([]byte, 12)
+	for {
+		if _, err := conn.Read(buf); err != nil {
+			log.Println(err)
+			return
+		}
+		joyX := math.Float32frombits(binary.BigEndian.Uint32(buf))
+		if joyX > 1.0 {
+			joyX = 1.0
+		} else if joyX < -1.0 {
+			joyX = -1.0
+		}
+		joyY := math.Float32frombits(binary.BigEndian.Uint32(buf[4:]))
+		if joyY > 1.0 {
+			joyY = 1.0
+		} else if joyY < -1.0 {
+			joyY = -1.0
+		}
+		buttons := binary.BigEndian.Uint32(buf[8:])
+		fmt.Println(joyX, joyY, buttons)
+	}
 }
 
-func main () {
-    log.SetFlags(0)
+func main() {
+	runtime.LockOSThread()
+	log.SetFlags(0)
 
-    if sdl.Init(sdl.INIT_EVERYTHING) != 0 {
-        log.Fatal(sdl.GetError())
-    }
+	if sdl.Init(sdl.INIT_EVERYTHING) != 0 {
+		log.Fatal(sdl.GetError())
+	}
 
-    var screen = sdl.SetVideoMode(800, 600, 32, sdl.RESIZABLE)
+	sdl.WM_SetCaption("Lecture Hall Games 0.1", "")
 
-    if screen == nil {
-        log.Fatal(sdl.GetError())
-    }
+	var screen = sdl.SetVideoMode(800, 600, 32, sdl.RESIZABLE)
 
-    rand.Seed(time.Now().UnixNano())
+	if screen == nil {
+		log.Fatal(sdl.GetError())
+	}
 
-    sdl.EnableUNICODE(1)
+	rand.Seed(time.Now().UnixNano())
 
-    ticker := time.NewTicker(time.Second / 50)
+	sdl.EnableUNICODE(1)
 
-    go func () {
-        ln, err := net.Listen("tcp", ":8080")
-        if err != nil {
-            log.Fatal("Server failed")
-        }
+	ticker := time.NewTicker(time.Second / 50)
 
-        for {
-            conn, err := ln.Accept()
-            if err != nil {
-                log.Fatal("Client failed")
-                continue
-            }
+	go func() {
+		ln, err := net.Listen("tcp", ":8080")
+		if err != nil {
+			log.Fatal("Server failed")
+		}
 
-            go handleConnection(conn)
-        }
-    }()
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				log.Fatal("Client failed")
+				continue
+			}
 
-    go func () {
-        for {
-            select {
-            case <- ticker.C:
-                screen.FillRect(nil, 0x302019)
-            loop:
-                for {
-                    select {
-                    default:
-                        break loop
-                    }
-                }
-                screen.Flip()
-            }
-        }
-    }()
+			go handleConnection(conn)
+		}
+	}()
 
-    running := true
-    for running {
-        select {
-        case _event := <-sdl.Events:
-            switch e := _event.(type) {
-            case sdl.QuitEvent:
-                running = false
-            case sdl.ResizeEvent:
-                screen = sdl.SetVideoMode(int(e.W), int(e.H), 32,
-                    sdl.RESIZABLE)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				screen.FillRect(nil, 0x302019)
+			loop:
+				for {
+					select {
+					default:
+						break loop
+					}
+				}
+				screen.Flip()
+			}
+		}
+	}()
 
-                if screen == nil {
-                    log.Fatal(sdl.GetError())
-                }
-            case sdl.KeyboardEvent:
-            }
-        }
-    }
+	running := true
+	for running {
+		select {
+		case _event := <-sdl.Events:
+			switch e := _event.(type) {
+			case sdl.QuitEvent:
+				running = false
+			case sdl.ResizeEvent:
+				screen = sdl.SetVideoMode(int(e.W), int(e.H), 32,
+					sdl.RESIZABLE)
 
-    sdl.Quit()
+				if screen == nil {
+					log.Fatal(sdl.GetError())
+				}
+			case sdl.KeyboardEvent:
+			}
+		}
+	}
+
+	sdl.Quit()
 }
-
-
