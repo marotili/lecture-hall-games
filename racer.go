78371406279d4c42e2d239019f08cc940a929cd4
Christoph Hack <christoph@tux21b.org>
2012-11-01 20:51:39 +0100

    fixed error

--- racer.go
+++ racer.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-    "github.com/banthar/gl"
-    "os"
+	"github.com/banthar/gl"
+	"image"
+	"image/color"
+	"image/png"
 	"math"
+	"os"
 	"time"
-    "image"
-    "image/png"
-    "image/color"
 )
 
 type Vector struct {
@@ -16,9 +16,9 @@ type Vector struct {
 }
 
 func (v *Vector) Normalize() {
-    length := float32(math.Sqrt(float64(v.x*v.x + v.y*v.y)))
-    v.x = v.x/length
-    v.y = v.y/length
+	length := float32(math.Sqrt(float64(v.x*v.x + v.y*v.y)))
+	v.x = v.x / length
+	v.y = v.y / length
 }
 
 func (lhs Vector) Mul(rhs Vector) float32 {
@@ -50,61 +50,60 @@ type Car struct {
 }
 
 func (car *Car) Draw(heightMod float32) {
-    angle := math.Pi/2.0 + math.Atan2(float64(car.direction.y), float64(car.direction.x))
-    car.sprite.Draw(float32(car.position.x), float32(car.position.y),
-    float32(angle), heightMod)
+	angle := math.Pi/2.0 + math.Atan2(float64(car.direction.y), float64(car.direction.x))
+	car.sprite.Draw(float32(car.position.x), float32(car.position.y),
+		float32(angle), heightMod)
 }
 
 type Sprite struct {
-    filename    string
-    width       int
-    height      int
-    texture gl.Texture
+	filename string
+	width    int
+	height   int
+	texture  gl.Texture
 }
 
 func NewSprite(filename string, width, height int) *Sprite {
-    fi, _ := os.Open(filename)
-    img, _ := png.Decode(fi)
-
+	fi, _ := os.Open(filename)
+	img, _ := png.Decode(fi)
 
 	gl.Enable(gl.TEXTURE_2D)
-    texture := gl.GenTexture()
-    texture.Bind(gl.TEXTURE_2D)
+	texture := gl.GenTexture()
+	texture.Bind(gl.TEXTURE_2D)
 
-    gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-    gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-    gl.TexImage2D(gl.TEXTURE_2D, 0, 4, img.Bounds().Max.X, img.Bounds().Max.Y, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-    (img.(*image.RGBA)).Pix)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.TexImage2D(gl.TEXTURE_2D, 0, 4, img.Bounds().Max.X, img.Bounds().Max.Y, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+		(img.(*image.RGBA)).Pix)
 	gl.Disable(gl.TEXTURE_2D)
 
-    return &Sprite{
-        filename : filename,
-        width : width,
-        height : height,
-        texture : texture,
-    }
+	return &Sprite{
+		filename: filename,
+		width:    width,
+		height:   height,
+		texture:  texture,
+	}
 }
 
 func (sprite *Sprite) Draw(x, y, angle, scale float32) {
-    gl.Enable(gl.TEXTURE_2D)
-    gl.MatrixMode(gl.MODELVIEW)
-    gl.LoadIdentity()
-    gl.Translatef(x, y, 0)
-    gl.Rotatef(angle*360/(2*math.Pi), 0, 0, 1)
-    gl.Scalef(scale, scale, 1)
-    sprite.texture.Bind(gl.TEXTURE_2D) 
-    gl.Begin(gl.QUADS)
-    gl.Color3f(1, 1, 1)
-    gl.TexCoord2d(0, 0)
-    gl.Vertex3f(-float32(sprite.width/2), -float32(sprite.height/2), 0)
-    gl.TexCoord2d(1, 0)
-    gl.Vertex2f(float32(sprite.width/2), -float32(sprite.height/2))
-    gl.TexCoord2d(1, 1)
-    gl.Vertex3f(float32(sprite.width/2), float32(sprite.height/2), 0)
-    gl.TexCoord2d(0, 1)
-    gl.Vertex3f(-float32(sprite.width/2), float32(sprite.height/2), 0)
-    gl.End()
-    gl.Disable(gl.TEXTURE_2D)
+	gl.Enable(gl.TEXTURE_2D)
+	gl.MatrixMode(gl.MODELVIEW)
+	gl.LoadIdentity()
+	gl.Translatef(x, y, 0)
+	gl.Rotatef(angle*360/(2*math.Pi), 0, 0, 1)
+	gl.Scalef(scale, scale, 1)
+	sprite.texture.Bind(gl.TEXTURE_2D)
+	gl.Begin(gl.QUADS)
+	gl.Color3f(1, 1, 1)
+	gl.TexCoord2d(0, 0)
+	gl.Vertex3f(-float32(sprite.width/2), -float32(sprite.height/2), 0)
+	gl.TexCoord2d(1, 0)
+	gl.Vertex2f(float32(sprite.width/2), -float32(sprite.height/2))
+	gl.TexCoord2d(1, 1)
+	gl.Vertex3f(float32(sprite.width/2), float32(sprite.height/2), 0)
+	gl.TexCoord2d(0, 1)
+	gl.Vertex3f(-float32(sprite.width/2), float32(sprite.height/2), 0)
+	gl.End()
+	gl.Disable(gl.TEXTURE_2D)
 }
 
 type Graymap struct {
@@ -112,25 +111,25 @@ type Graymap struct {
 }
 
 func (graymap Graymap) Modifier(pos Vector) float32 {
-    color := graymap.data.At(int(pos.x), int(pos.y)).(color.Gray)
-	return float32(color.Y)/float32(255)
+	color := graymap.data.At(int(pos.x), int(pos.y)).(color.Gray)
+	return float32(color.Y) / float32(255)
 }
 
 type Racer struct {
 	cars            []*Car
 	boundingRects   []Vector // stub
 	velocityGraymap Graymap
-    heightGraymap   Graymap
+	heightGraymap   Graymap
 }
 
 func NewRacer() *Racer {
-    fi, _ := os.Open("artwork/velocity.png")
-    img, _ := png.Decode(fi)
+	fi, _ := os.Open("artwork/velocity.png")
+	img, _ := png.Decode(fi)
 	return &Racer{
 		cars:            nil,
 		boundingRects:   nil,
 		velocityGraymap: Graymap{(img.(*image.Gray))},
-        heightGraymap:Graymap{(img.(*image.Gray))},
+		heightGraymap:   Graymap{(img.(*image.Gray))},
 	}
 }
 
@@ -143,23 +142,23 @@ func NewCar(owner Player, sprite *Sprite) *Car {
 		zLevel:      0,
 		layer:       0,
 		owner:       owner,
-        sprite: sprite,
+		sprite:      sprite,
 	}
 }
 
 var timeFactor float32 = 0.00000001
 
 func (car *Car) steer(power float32, elapsedTime time.Duration) {
-	max_angle := float32(math.Pi / 4.0)
+	max_angle := float32(math.Pi / 8.0)
 	car.direction.x =
-    car.direction.x*float32(math.Cos(float64(max_angle*power*float32(elapsedTime)*timeFactor))) -
-		car.direction.y*float32(math.Sin(float64(max_angle*power*float32(elapsedTime)*timeFactor)))
+		car.direction.x*float32(math.Cos(float64(max_angle*power*float32(elapsedTime)*timeFactor))) -
+			car.direction.y*float32(math.Sin(float64(max_angle*power*float32(elapsedTime)*timeFactor)))
 
 	car.direction.y =
-    car.direction.x*float32(math.Sin(float64(max_angle*power*float32(elapsedTime)*timeFactor))) +
-		car.direction.y*float32(math.Cos(float64(max_angle*power*float32(elapsedTime)*timeFactor)))
+		car.direction.x*float32(math.Sin(float64(max_angle*power*float32(elapsedTime)*timeFactor))) +
+			car.direction.y*float32(math.Cos(float64(max_angle*power*float32(elapsedTime)*timeFactor)))
 
-    car.direction.Normalize()
+	car.direction.Normalize()
 }
 
 func (racer *Racer) Update(elapsedTime time.Duration) {
@@ -167,14 +166,14 @@ func (racer *Racer) Update(elapsedTime time.Duration) {
 		car.velocity =
 			car.maxVelocity * racer.velocityGraymap.Modifier(car.position)
 		car.position =
-        car.position.Add(car.direction.MulScalar(car.velocity*(float32(elapsedTime))*timeFactor))
+			car.position.Add(car.direction.MulScalar(car.velocity * (float32(elapsedTime)) * timeFactor))
 	}
 }
 
 func (racer *Racer) Render() {
 	for _, car := range racer.cars {
-//        heightMod := 1/racer.heightGraymap.Modifier(car.position)
-        car.Draw(heightMod)
+		heightMod := float32(1.0) // 1/racer.heightGraymap.Modifier(car.position)
+		car.Draw(heightMod)
 	}
 	// background layer
 	// map layers
