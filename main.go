bac48b7bd4cd0a010f17ac9d8b1b5801a2dbffd4
Marco Fruhwirth <marco.fruhwirth@student.tugraz.at>
2012-11-01 17:11:38 +0100

    merged with remote

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"github.com/0xe2-0x9a-0x9b/Go-SDL/sdl"
+	"github.com/banthar/gl"
 	"log"
 	"math"
 	"math/rand"
@@ -48,26 +49,45 @@ func handleConnection(conn net.Conn) {
 	}
 }
 
+func loadTexture() {
+	gl.Enable(gl.TEXTURE_2D)
+	gl.Disable(gl.TEXTURE_2D)
+}
+
 func main() {
-	runtime.LockOSThread()
 	log.SetFlags(0)
+	runtime.LockOSThread()
 
 	if sdl.Init(sdl.INIT_EVERYTHING) != 0 {
+		log.Fatal(sdl.GetError())
+	}
+
+	var screen = sdl.SetVideoMode(800, 600, 32, sdl.OPENGL)
+	if screen == nil {
 		log.Fatal(sdl.GetError())
 	}
 
 	sdl.WM_SetCaption("Lecture Hall Games 0.1", "")
 	sdl.EnableUNICODE(1)
 
-	screen := sdl.SetVideoMode(800, 600, 32, sdl.RESIZABLE)
-	if screen == nil {
-		log.Fatal(sdl.GetError())
+	if gl.Init() != 0 {
+		panic("gl error")
+
 	}
+
+	gl.MatrixMode(gl.PROJECTION)
+	gl.Viewport(0, 0, int(screen.W), int(screen.H))
+	gl.LoadIdentity()
+	gl.Ortho(0, float64(screen.W), float64(screen.H), 0, -1.0, 1.0)
+	gl.ClearColor(1, 1, 1, 0)
+	gl.Clear(gl.COLOR_BUFFER_BIT)
 
 	rand.Seed(time.Now().UnixNano())
 
+	sdl.EnableUNICODE(1)
+
 	go func() {
-		ln, err := net.Listen("tcp", ":8080")
+		ln, err := net.Listen("tcp", ":8001")
 		if err != nil {
 			log.Fatal("Server failed")
 		}
@@ -84,7 +104,8 @@ func main() {
 	}()
 
 	running := true
-	last := time.Now()
+    last := time.Now()
+
 	for running {
 
 		// process events
@@ -98,15 +119,13 @@ func main() {
 			}
 		default:
 		}
-
 		// move objects
 		current := time.Now()
 		t := current.Sub(last)
 		last = current
 		fmt.Println(t)
 
-		// render screen
-		screen.FillRect(nil, 0x302019)
+		sdl.GL_SwapBuffers()
 	}
 
 	sdl.Quit()
--- racer.go
+++ racer.go
@@ -1,4 +1,102 @@
 package main
 
+import (
+	"github.com/0xe2-0x9a-0x9b/Go-SDL/sdl"
+	"log"
+	"math"
+	"time"
+)
+
+type Vector struct {
+	x float32
+	y float32
+}
+
+func (lhs Vector) Mul(rhs Vector) float32 {
+	return lhs.x*rhs.x + lhs.y + rhs.y
+}
+
+func (lhs Vector) MulScalar(rhs float32) Vector {
+	return Vector{lhs.x * rhs, lhs.y * rhs}
+}
+
+func (lhs Vector) Add(rhs Vector) Vector {
+	return Vector{lhs.x + rhs.x, lhs.y + rhs.y}
+}
+
+type Player struct {
+	name string
+}
+
 type Car struct {
+	position    Vector
+	direction   Vector
+	maxVelocity float32
+	velocity    float32
+	zLevel      int
+	layer       int
+	owner       Player
+	surface     *sdl.Surface
+}
+
+type Greymap struct {
+	data *sdl.Surface
+}
+
+func (greymap Greymap) Modifier(pos Vector) float32 {
+	value := getpixel(greymap.data, uint16(pos.x), uint16(pos.y))
+	return float32(255) / float32(value)
+}
+
+type Racer struct {
+	cars            []*Car
+	boundingRects   []Vector // stub
+	velocityGreymap Greymap
+}
+
+func NewRacer() *Racer {
+	return &Racer{
+		cars:            nil,
+		boundingRects:   nil,
+		velocityGreymap: Greymap{sdl.Load("artwork/velocity.png")},
+	}
+}
+
+func NewCar(owner Player) *Car {
+	return &Car{
+		position:    Vector{0, 0},
+		direction:   Vector{1, 0},
+		maxVelocity: 10,
+		velocity:    10,
+		zLevel:      0,
+		layer:       0,
+		owner:       owner,
+	}
+}
+
+func (car *Car) steer(power float32) {
+	max_angle := float32(math.Pi / 4.0)
+	car.direction.x = car.direction.x*float32(math.Cos(float64(max_angle*power))) -
+		car.direction.y*float32(math.Sin(float64(max_angle*power)))
+
+	car.direction.y = car.direction.x*float32(math.Sin(float64(max_angle*power))) +
+		car.direction.y*float32(math.Cos(float64(max_angle*power)))
+}
+
+func (racer *Racer) Update(elapsedTime time.Duration) {
+	for _, car := range racer.cars {
+		car.velocity =
+			car.maxVelocity * racer.velocityGreymap.Modifier(car.position)
+		car.position = car.position.Add(car.direction.MulScalar(car.velocity))
+	}
+}
+
+func (racer *Racer) Render(screen *sdl.Surface) {
+	screen.FillRect(nil, 0x000000)
+	for _, car := range racer.cars {
+		log.Fatal("%f", car.velocity)
+	}
+	// background layer
+	// map layers
+	// cars
 }
